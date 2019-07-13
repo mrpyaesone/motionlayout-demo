3eeeec63ce8130abaa9fcf8846542d96952bebcf
Myo Thant <myothantktt@gmail.com>
2019-07-13 18:10:29 +0630

    Handling transition event ( progressing SeekBar's progress while triggering animation )

--- app/src/main/java/com/ps/motionlayout_demo/MainActivity.kt
+++ app/src/main/java/com/ps/motionlayout_demo/MainActivity.kt
@@ -2,14 +2,37 @@ package com.ps.motionlayout_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        motion_container.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionTrigger(motionLayout: MotionLayout?, currentId: Int, p2: Boolean, p3: Float) {
+            }
+
+            override fun onTransitionStarted(motionLayout: MotionLayout?, currentId: Int, p2: Int) {
+            }
+
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+                Log.d("motionlayout_demo", "motion Progress : " + progress)
+                Log.d("motionlayout_demo", "seekbar_Progress : " + ceil(progress * 100))
+                seekbar.progress = ceil(progress * 100).toInt()
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                if (currentId == R.id.ending_set) {
+                    motion_container.transitionToStart()
+                }
+            }
+        })
     }
 
     fun start(v: View) {

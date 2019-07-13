89f376b7b067cc61f74aff9a5d21f2efc23c99ab
Myo Thant <myothantktt@gmail.com>
2019-07-13 17:37:22 +0630

    First step of MotionLayout ( MotionLayout, MotionScene, ConstraintSet, Transaction )

--- app/src/main/java/com/ps/motionlayout_demo/MainActivity.kt
+++ app/src/main/java/com/ps/motionlayout_demo/MainActivity.kt
@@ -2,11 +2,17 @@ package com.ps.motionlayout_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun start(v: View) {
+        motion_container.transitionToEnd()
     }
 }
